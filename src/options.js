c4bc5f84a7c5883437c2b4cb1113f193b2a3d898
Dan <dstein64@users.noreply.github.com>
2020-09-12 01:36:45 -0400

    Decouple label for touch devices, since clicking shows the tooltip.

--- src/options.js
+++ src/options.js
@@ -196,6 +196,14 @@ if (numHighlightStates < 3) {
     }
 }
 
+// decouple label for touch devices, since clicking shows the tooltip.
+if (window.matchMedia('(pointer: coarse)').matches) {
+    let labels = document.getElementsByClassName('mobile-remove-for');
+    for (let i = 0; i < labels.length; ++i) {
+        labels[i].removeAttribute('for');
+    }
+}
+
 // save options on any user input
 (function() {
     highlightColorInput.addEventListener('change', propagateOptions);
